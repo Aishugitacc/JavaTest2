5198b9c57619900c80d776379ec1c311683af226
Aishugitacc <aishuravinagulan19@gmail.com>
2021-11-23 10:36:30 +0530

    Task2

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -2,6 +2,7 @@ package org.bank;
 
 public class AxisBank {
  public void current() {
+	 System.out.println("*******************");
 	 System.out.println(" current account no: 8242376837568");
 }
 }
